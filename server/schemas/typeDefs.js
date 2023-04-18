bebdf1cd755e4bca0233e311af563506b009be69
smulch <mulcahy.samuel@gmail.com>
2023-04-18 13:33:23 -0400

    added initial typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,10 +6,35 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
+    entries: [Entry]
   }
-
+  type Entry {
+    _id: ID
+    game: String
+    user: User
+    datePlayed: String
+    platform: String
+    review: String
+    createdAt: String
+    updatedAt: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
   type Query {
     users: [User]
+    user(username: String!): User
+    entries(username: String): [Entry]
+    entry(entryId: ID!): Entry
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addEntry(game: String!, user: String!, datePlayed: String, platform: String, review: String): Entry
+    updateUser(userId: ID!, username: String, email: String, password: String): User
+    updateEntry(entryId: ID!, datePlayed: String, platform: String, review: String): Entry
+    removeUser(userId: ID!): User
+    removeEntry(entryId: ID!): Entry
   }
 `;
 
