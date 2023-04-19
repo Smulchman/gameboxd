1499a70c52b1794097c32ac8a665e43f1ee062b0
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-19 12:29:22 -0400

    adds games resolver to get api calls

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,6 +12,20 @@ const resolvers = {
         },
       ];
     },
+    games: async () => {
+      const data = await axios.get(
+        'https://rawg-video-games-database.p.rapidapi.com/games?key=5cb5074085274b3aab2431311200438c',
+        {
+          headers: {
+            'x-rapidapi-key':
+              '87cdee1fecmsha53138e19c8fc31p120979jsnc537093c677e',
+            'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+          },
+        }
+      );
+      const game = data.data;
+      return game;
+    },
   },
 };
 
