3478c3fa621e064b4ed5568751be709ebd882a05
smulch <mulcahy.samuel@gmail.com>
2023-04-19 13:38:47 -0400

    early work on gamedata virtual

--- server/models/Entry.js
+++ server/models/Entry.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const entrySchema = new Schema({
   game: {
-    type: String,
+    type: Number,
     required: true,
   },
   user: {
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,9 @@ const resolvers = {
       return await User.find({});
     },
     entries: async (parent, { username }) => {
+      // query your mongoose model for entries
+      // map through entries and make api calls for game data associated with each entry
+      // return all the data to the client
       return await Entry.find({ username });
     },
   },
