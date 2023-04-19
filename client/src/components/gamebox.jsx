4ddf9d8a019d6891ae6d1c199ff976f5c408dc4b
Alien-oyi <daijinmin78@gmail.com>
2023-04-19 18:38:48 -0400

    pull

--- client/src/components/gamebox.jsx
+++ client/src/components/gamebox.jsx
@@ -5,6 +5,7 @@ import { getGames } from '../API/rawgapi';
 import { useEffect, useState } from 'react';
 import "../assets/css/gamebox.css"
 
+
 function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
@@ -24,28 +25,35 @@ export default function QuiltedImageList() {
       }
       fetchData();
     }, []);
+    const handleClick = (game) => {
+        // handle the click event here, for example, navigate to a different page
+        window.location.href=(`https://rawg.io/games/${game.id}`);
+        
+    };
   return (
     <div className="gamebox">
-    <ImageList
-      sx={{ width: "60%", height: 450 }}
-      variant="quilted"
-      cols={4}
-      rowHeight={121}
-    >
-      {games.map((game) => (
-        <ImageListItem
-          key={game.img}
-          cols={Math.floor(Math.random() * 3)  || 1} 
-          rows={Math.floor(Math.random() * 3)  || 1}
-        >
-          <img
-            {...srcset(game.background_image, 121, game.rows, game.cols)}
-            alt={game.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+      <ImageList
+        sx={{ width: '60%', height: 450 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
+        {games.map((game) => (
+          <ImageListItem
+            key={game.img}
+            cols={Math.floor(Math.random() * 3) || 1}
+            rows={Math.floor(Math.random() * 3) || 1}
+          >
+            <img
+              {...srcset(game.background_image, 121, game.rows, game.cols)}
+              alt={game.title}
+              loading="lazy"
+              style={{ cursor: 'pointer' }}
+              onClick={() => handleClick(game)}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
