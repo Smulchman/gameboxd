697bd3a3b1e8f97f88b0c47e431fb16e7142c95f
joseph reis <josephreis646@gmail.com>
2023-04-19 14:57:18 -0400

    fixed bugs related to API integration

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+// .env
+require('dotenv').config({ path: "../.env" });
+console.log('RAWG_API_KEY', process.env.RAWG_API_KEY)
+// import dotenv from "dotenv";
+
 // express
 const express = require('express');
 const path = require('path');
@@ -10,6 +15,8 @@ const db = require('./config/connection');
 const api = require('./utils/rawgAPI.js');
 // Axios 
 const axios = require('axios');
+// import function 
+const {getGames} = require('./utils/rawgAPI.js'); 
 
 
 // graphql schemas
@@ -31,31 +38,14 @@ app.use(
   })
 );
 
+getGames();
+
 // point to dist folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', (req, res) =>
   res.sendFile(__dirname, '../client/dist/index.html')
 );
-
-// Endpoint to retrieve data from RAWG API
-app.get('/rawg-games', async (req, res) => {
-  const apiKey = process.env.RAWG_API_KEY;
-  const endpointUrl = 'https://rawg-video-games-database.p.rapidapi.com/games';
-  const headers = {
-    'Content-Type': 'application/json',
-    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-    'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
-    'Authorization': `Bearer ${apiKey}`
-  };
-  try {
-    const response = await axios.get(`${endpointUrl}?key=${apiKey}`, { headers });
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve RAWG games data' });
-  }
-});
 
 // startserver
 const startServer = async () => {
--- server/utils/rawgAPI.js
+++ server/utils/rawgAPI.js
@@ -1,18 +1,29 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const apiKey = process.env.RAWG_API_KEY;
-const endpointUrl = "https://api.rawg.io/api/games";
-const headers = {
-  "Content-Type": "application/json",
-  "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-  "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
-};
+const RAWG_API_KEY = 
+process.env.RAWG_API_KEY;
 
-axios
-  .get(`${endpointUrl}?key=${apiKey}`, { headers })
-  .then((response) => {
+const RAPID_API_KEY = 
+process.env.RAPID_API_KEY;
+
+const RAPID_API_HOST = 'rawg-video-games-database.p.rapidapi.com';
+const API_URL = 'https://rawg-video-games-database.p.rapidapi.com/games';
+
+async function getGames() {
+  try {
+    const response = await axios.get(`${API_URL}?key=${RAWG_API_KEY}`,{
+      headers: {
+        'x-rapidapi-key': RAPID_API_KEY,
+        'x-rapidapi-host': RAPID_API_HOST,
+      }
+    });
+    const data = response.data;
     console.log(response.data);
-    // process the data here
-  })
-  .catch((error) => console.error(error));
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = {getGames};
 
