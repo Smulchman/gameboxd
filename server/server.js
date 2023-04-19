efa10c8a9df023d05799d9294832d486a7033648
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-19 11:58:17 -0400

    nothing commit

--- server/server.js
+++ server/server.js
@@ -27,27 +27,6 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  const apiKey = '5cb5074085274b3aab2431311200438c';
-  const endpointUrl = 'https://api.rawg.io/api/games';
-  const headers = {
-    'Content-Type': 'application/json',
-    'X-RapidAPI-Key': '87cdee1fecmsha53138e19c8fc31p120979jsnc537093c677e',
-    'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
-  };
-
-  axios
-    .get(`${endpointUrl}?key=${apiKey}`, { headers })
-    .then((response) => {
-      console.log(response.data);
-      // process the data here
-    })
-    .catch((error) => console.error(error));
-});
-=======
->>>>>>> 249033d749b91dd803b427c4a47c72af750a8d29
-
 // point to dist folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
