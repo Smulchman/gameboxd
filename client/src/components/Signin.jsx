de7926bd66a5fb59fd06faf425e80098b9d838c4
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-19 10:09:15 -0400

    added sign up and css

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -11,7 +11,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import '../assets/css/Signin.css';
+
 
 // const theme = createTheme();
 
