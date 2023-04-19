f5b202397659e66d74179165bc473a07d5ac8988
Alien-oyi <daijinmin78@gmail.com>
2023-04-19 12:54:41 -0400

    gamebox.jsx and gamebox.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import GameList from './components/getgame';
-import BasicImageList from './components/home';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import QuiltedImageList from './components/gamebox';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -19,7 +19,7 @@ function App() {
     <ApolloProvider client={client}>
       <Navbar />
       <Home />
-      <BasicImageList />
+      <QuiltedImageList />     
       <Footer />
     </ApolloProvider>
   );
