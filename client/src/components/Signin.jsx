7460d4043d2b4918ca62b67bd5af69df6f40caa0
jake rudlong <jrudlong@gmail.com>
2023-04-20 17:17:28 -0500

    adds auth.js to signin component, tweaks a couple other things

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -13,7 +13,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { LOGIN_USER } from '../utils/mutations';
-// import { Link } from 'react-router-dom';
+import Auth from '../utils/auth'
 
 
 // const theme = createTheme();
