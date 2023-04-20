95338210d7c72f5c7be0860b02bcc7a0ff0dc296
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-20 11:54:12 -0400

    fixes isCorrectPassword function in User.js by adding a return

--- server/models/User.js
+++ server/models/User.js
@@ -45,7 +45,7 @@ userSchema.pre('save', async function (next) {
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-  await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
