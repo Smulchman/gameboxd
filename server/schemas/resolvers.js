73f260736c683b97dbf7f14b01dfeae620eba638
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-20 11:11:17 -0400

    creates mutation area in resolvers and creates addUser function in Mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -26,6 +26,15 @@ const resolvers = {
       return game;
     },
   },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
