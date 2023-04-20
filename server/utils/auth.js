dabdb2ad1e74a5f28c4c176c09c4e1f2b97dd685
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-20 10:53:41 -0400

    adds logic to the authMiddleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,5 +5,22 @@ const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: function ({ req }) {},
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.token;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid Token');
+    }
+  },
 };
