02f10336b4e60ae86e71c0690c2e13de025a0cc8
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-20 11:17:08 -0400

    creates login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -34,6 +34,23 @@ const resolvers = {
 
       return { token, user };
     },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('The infomation is incorrect');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('The infomation is incorrect');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
   },
 };
 
