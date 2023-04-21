01a901b085d3602704312535c936697f3ad522df
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-21 16:08:40 -0400

    fixing blockers

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,79 +1,103 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+// import { idbPromise } from '../../utils/helpers';
 // import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+// import { useStoreContext } from '../../utils/GlobalState';
+// import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 // import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-const Cart = () => { 
-  const [state, dispatch] = useStoreContext();
+const Cart = () => {
+  const [donationFormData, setDonationFormData] = useState({
+    user: '',
+    description: '',
+    amount: 0,
+  });
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-  useEffect(() => {
-    if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
-    }
-  }, [data]);
-  useEffect(() => {
-    async function getCart() {
-      const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    }
-
-    if (!state.cart.length) {
-      getCart();
-    }
-  }, [state.cart.length, dispatch]);
-
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
-
-  function calculateTotal() {
-    let sum = 0;
-    state.cart.forEach((item) => {
-      sum += item.price * item.purchaseQuantity;
+    setDonationFormData({
+      ...donationFormData,
+      [name]: value,
     });
-    return sum.toFixed(2);
-  }
+  };
 
   function submitCheckout() {
-    const productIds = [];
-
-    state.cart.forEach((item) => {
-      for (let i = 0; i < item.purchaseQuantity; i++) {
-        productIds.push(item._id);
-      }
-    });
-
     getCheckout({
       variables: { products: productIds },
     });
   }
 
-// if (!state.cartOpen) {
-//     return (
-//       <div className="cart-closed" onClick={toggleCart}>
-//         <span
-//           role="img"
-//           aria-label="trash">🛒</span>
-//       </div>
-//     );
-//   }
+  // if (!state.cartOpen) {
+  //     return (
+  //       <div className="cart-closed" onClick={toggleCart}>
+  //         <span
+  //           role="img"
+  //           aria-label="trash">🛒</span>
+  //       </div>
+  //     );
+  //   }
 
   return (
     <div className="cart">
-      <div className="close" onClick={toggleCart}>[close]</div>
-      <h2>Shopping Cart</h2>
-      {state.cart.length ? (
+      {/* <div className="close" onClick={toggleCart}>[close]</div> */}
+      <h2>Donations</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          className="form-input"
+          placeholder="username"
+          name="user"
+          type="text"
+          value={donationFormData.user}
+          onChange={handleChange}
+        />
+        <input
+          className="form-input"
+          placeholder="description"
+          name="description"
+          type="text"
+          value={donationFormData.description}
+          onChange={handleChange}
+        />
+        <form onSubmit={submitCheckout}>
+          <input
+            className="form-input"
+            placeholder="Your email"
+            name="email"
+            type="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            name="amount"
+            type="range"
+            value={donationFormData.amount}
+            onChange={handleChange}
+          />
+          <button
+            className="btn btn-block btn-primary"
+            style={{ cursor: 'pointer' }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+        <button
+          className="btn btn-block btn-primary"
+          style={{ cursor: 'pointer' }}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      {/* {state.cart.length ? (
         <div>
           {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
@@ -97,9 +121,9 @@ const Cart = () => {
             </span>
             You haven't added anything to your cart yet!
           </h3>
-      )}
+      )} */}
     </div>
   );
 };
 
-    export default Cart;
+export default Cart;
