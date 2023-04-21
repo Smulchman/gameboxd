0f57138d22e279dc026e392be6cf313fb0cecb85
jake rudlong <jrudlong@gmail.com>
2023-04-20 21:43:23 -0500

    styles search results page

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -1,86 +1,54 @@
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import GameReviewCard from '../components/GameReviewCard';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '50ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-// const style = {
-//   position: 'absolute',
-//   top: '50%',
-//   left: '50%',
-//   transform: 'translate(-50%, -50%)',
-//   width: 400,
-//   bgcolor: 'background.paper',
-//   border: '2px solid #000',
-//   boxShadow: 24,
-//   p: 4,
-// };
 
 export default function SearchResults() {
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Search className="searchBar">
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </Search>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <GameReviewCard />
-    </>
+    <div style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      background: '#292827'
+    }}>
+      <div style={{ 
+        display: 'flex', 
+        width: '100%', 
+        justifyContent: 'center',
+        margin: '1em' 
+      }}>
+       <FormControl style={{ 
+          width: '60%' 
+        }} variant="standard">
+          <h3 style={{color: 'white', fontSize: '2em'}}>Search a game to review!</h3>
+        <Input
+        style={{ 
+          background: 'white',
+          height: '3em',
+          borderRadius: '2em',
+          fontSize: '1.5em' 
+        }}
+          id="gameSearch"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon style={{margin: '2em', fontSize: '2em'}} />
+            </InputAdornment>
+          }
+        />
+       </FormControl>
+      </div>
+      <GameReviewCard style={{ marginTop: '2em'}} />
+    </div>
   );
 }
