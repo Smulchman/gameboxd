1c1479c90a0c39a2535b58fc59f382bd146a1935
Jared A <jroberts1931@gmail.com>
2023-04-22 00:00:26 -0400

    border on bottom of recent list

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ import Auth from '../utils/auth.js';
 import { useQuery } from '@apollo/client';
 import { GET_ENTRIES } from '../utils/queries.js';
 import Entries from '../components/Entries.jsx';
-import '../assets/css/Home.css'
+import '../assets/css/Home.css';
 
 // modal stuff
 const style = {
@@ -39,16 +39,16 @@ export default function SimpleContainer() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [entryData, setEntryData] = useState([]);
-//  query to get game review entries
+  //  query to get game review entries
   const { loading, data, error } = useQuery(GET_ENTRIES, {
     variables: { limit: 10 },
   });
-// checks to see if there is data from query --> sets entryData state with the array of entries
+  // checks to see if there is data from query --> sets entryData state with the array of entries
   useEffect(() => {
     const getEntries = () => {
-      if (data && !loading ) {
-        const entries = data.entries.slice(0,10);
-        setEntryData(entries)
+      if (data && !loading) {
+        const entries = data.entries.slice(0, 10);
+        setEntryData(entries);
       }
     };
     if (data && !loading) {
@@ -169,6 +169,7 @@ export default function SimpleContainer() {
               createdAt={entry.createdAt}
               genre={entry.genres}
               platform={entry.platforms}
+              className={index === entryData.length - 1 ? 'last-entry' : ''}
             />
           ))}
         </div>
