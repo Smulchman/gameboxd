32a15b9c0c9ec83f252416583c4f45b24adb8c1f
Jared A <jroberts1931@gmail.com>
2023-04-22 16:46:22 -0400

    Fixed background on profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,20 +15,20 @@ export default function Profile() {
   const me = Auth.getProfile();
   // initiate variable to store name from query results
   let myName;
-  // nested two layers in is email 
+  // nested two layers in is email
   const myEmail = me.data.email;
   // get user by email query
   const userQuery = useQuery(GET_USER, {
-    variables: {email: me.data.email}
-  })
+    variables: { email: me.data.email },
+  });
 
   // take the email from decoded jwt and query user
   const entryQuery = useQuery(GET_ENTRIES_BY_USER, {
-    variables: {email: myEmail}
-  })
+    variables: { email: myEmail },
+  });
   // if query for user info works, set profile username
   if (userQuery.data && !userQuery.loading) {
-  myName = userQuery.data.user.username
+    myName = userQuery.data.user.username;
   }
 
   useEffect(() => {
@@ -38,59 +38,56 @@ export default function Profile() {
         setUserEntries(entries.entries);
       }
     };
-    if(entryQuery.data) {
+    if (entryQuery.data) {
       getEntries();
     }
-  }, [entryQuery.data, entryQuery.loading] );
+  }, [entryQuery.data, entryQuery.loading]);
 
   console.log(userEntries);
 
   // the JSX
   return (
-    <div
-    style={{backgroundColor: '#282827', height: '100vh', padding: '1em'}} 
-    >
-      <Card sx={{ minWidth: 275, display: 'flex', justifyContent: 'center' }}
-      style={{backgroundColor: '#282827', color: 'white', border: '2px solid white'}}
+    <div style={{ backgroundColor: '#282827', height: '100%', padding: '1em' }}>
+      <Card
+        sx={{ minWidth: 275, display: 'flex', justifyContent: 'center' }}
+        style={{
+          backgroundColor: '#282827',
+          color: 'white',
+          border: '2px solid white',
+        }}
       >
         <CardContent>
-          <h2>
-            Welcome to your profile:
-          </h2>
-          <h2>
-            {myName}
-          </h2>
-          <h3>
-            {myEmail}
-          </h3>
+          <h2>Welcome to your profile:</h2>
+          <h2>{myName}</h2>
+          <h3>{myEmail}</h3>
         </CardContent>
       </Card>
-    {entryQuery.data && (
-      <div>
-        <h2
-        style={{
-          textAlign: 'center',
-          backgroundColor: '#292827',
-          color: 'white',
-          fontSize: '2em',
-        }}
-        > 
-          Your saved games and reviews:
-        </h2>
+      {entryQuery.data && (
+        <div>
+          <h2
+            style={{
+              textAlign: 'center',
+              backgroundColor: '#292827',
+              color: 'white',
+              fontSize: '2em',
+            }}
+          >
+            Your saved games and reviews:
+          </h2>
 
-        {userEntries.map((entry, index) => (
-          <SingleUserEntries 
-          key={index}
-          image={entry.gameData.background_image}
-          game={entry.gameData.name}
-          description={entry.gameData.description_raw}
-          platform={entry.platform}
-          review={entry.review}
-          createdAt={entry.createdAt}
-          />
-      ))}
-      </div>
-  )}
+          {userEntries.map((entry, index) => (
+            <SingleUserEntries
+              key={index}
+              image={entry.gameData.background_image}
+              game={entry.gameData.name}
+              description={entry.gameData.description_raw}
+              platform={entry.platform}
+              review={entry.review}
+              createdAt={entry.createdAt}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
