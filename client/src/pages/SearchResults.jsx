473caac5bf4820662fc28a46adcb4cb30808ec35
smulch <mulcahy.samuel@gmail.com>
2023-04-23 21:25:51 -0400

    changed the images to be square

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -19,9 +19,6 @@ export default function SearchResults() {
       [name]: value,
     });
   };
-  const handleClick = () => {
-    getGameData();
-  };
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
